f816c85a19a6c193f164d4a891d170f536e5a11b
Deepkeng <346755353@qq.com>
2016-09-02 10:26:03 +0800

    3

---
+++ app/src/main/java/com/example/xposedmudulehook/GetInputText.java
@@ -0,0 +1,30 @@
+package com.example.xposedmudulehook;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by psq on 2016/9/2
+ */
+public class GetInputText implements IXposedHookLoadPackage{
+    //boolean	commitText(CharSequence text, int newCursorPosition)
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        XposedHelpers.findAndHookMethod("android.view.inputmethod.BaseInputConnection", loadPackageParam.classLoader, "commitText", CharSequence.class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                CharSequence text = (CharSequence) param.args[0];
+                String s = text.toString();
+                XposedBridge.log("Text"+s);
+            }
+        });
+    }
+}
